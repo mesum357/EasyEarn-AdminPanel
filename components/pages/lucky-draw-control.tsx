f70195cadad3a80c9003b7c26759d910acbbee43
Mesum Abbas <i.mesumabbas#@gmail.com>
2025-08-03 18:50:04 +0500

    Fix CORS issue: Remove invalid Access-Control-Allow-Origin header from lucky draw requests

--- components/pages/lucky-draw-control.tsx
+++ components/pages/lucky-draw-control.tsx
@@ -46,7 +46,6 @@ export default function LuckyDrawControl() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify({
           ...formData,
